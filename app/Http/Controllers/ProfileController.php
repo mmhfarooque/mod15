b5cdff012293023340881e44d13bc8ecd0fe9933
mahmudfarooque <farooque7@gmail.com>
2024-09-28 22:01:33 +0600

    1
    1

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+        $name = "Donal Trump";
+        $age = "75";
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        return response()->json($data, 200)->cookie(
+            $cookieName,
+            $cookieValue,
+            $minutes,
+            $path,
+            $domain,
+            $secure,
+            $httpOnly
+        );
+    }
+}
